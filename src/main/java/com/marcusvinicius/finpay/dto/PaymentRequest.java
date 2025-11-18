04eaea29cc5d076e853fb8befc9fc5a59e9e8849
mvalbuquerque1 <mv.albuquerque1@gmail.com>
2025-11-17 21:11:30 -0300

    Convert PaymentRequest in record

--- src/main/java/com/marcusvinicius/finpay/dto/PaymentRequest.java
+++ src/main/java/com/marcusvinicius/finpay/dto/PaymentRequest.java
@@ -1,5 +1,6 @@
 package com.marcusvinicius.finpay.dto;
 
+import com.marcusvinicius.finpay.domain.Transaction;
 import com.marcusvinicius.finpay.util.enums.PaymentMethod;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
@@ -9,34 +10,39 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 import java.util.UUID;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-public class PaymentRequest {
-
-    @NotNull
-    private UUID userId;
-
-    @DecimalMin(value = "0.01", message = "Amount must be greater than zero")
-    private BigDecimal amount;
-
-    @NotNull
-    //TODO Create validation
-    @Pattern(regexp = "^(R\\$|\\$)$", message = "Currency must be R$ or $")
-    private String currency;
-
-    @NotNull
-    private PaymentMethod paymentMethod;
-
-    public PaymentRequest() {
+public record PaymentRequest(
+        @NotNull UUID userId,
+        @NotNull @DecimalMin(value = "0.01", message = "Amount must be greater than zero") BigDecimal amount,
+        @NotNull @Pattern(regexp = "^(R\\$|\\$)$", message = "Currency must be R$ or $") String currency,
+        @NotNull PaymentMethod paymentMethod
+) {
+    public PaymentRequest {
+        if (userId == null) {
+            throw new IllegalArgumentException("UserId must not be null");
+        }
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than zero");
+        }
+        if (currency == null || currency.isBlank()) {
+            throw new IllegalArgumentException("Currency must not be null or blank");
+        }
+        if (paymentMethod == null) {
+            throw new IllegalArgumentException("PaymentMethod must not be null");
+        }
+    }
+    public Transaction toDomain() {
+        Currency domainCurrency = mapToCurrency(this.currency);
+        return Transaction.pending(this.amount, domainCurrency);
     }
 
-    public PaymentRequest(UUID userId, BigDecimal amount, String currency, PaymentMethod paymentMethod) {
-        this.userId = userId;
-        this.amount = amount;
-        this.currency = currency;
-        this.paymentMethod = paymentMethod;
+    private Currency mapToCurrency(String symbol) {
+        return switch (symbol) {
+            case "R$" -> Currency.getInstance("BRL");
+            case "$" -> Currency.getInstance("USD");
+            default -> throw new IllegalArgumentException("Unsupported currency symbol: " + symbol);
+        };
     }
 }
